1ea3ab978332f547c54dd688664e8fd14c380cdf
estebanRicardo <esteban.ricardo.dev@gmail.com>
2025-09-11 11:18:44 -0500

    Caracteristica: Se agrego ctrud de dataUser.
    detalles del cambio:
    
    - Se realizo crud basico en dataUser validando por el id del usuario.
    
    Por: Esteban David Ricardo Fuentes.
    Fecha: 11/09/2025.
    Version: 0.4.0

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,13 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserDataController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::post('/register', [AuthController::class, 'register']);
-
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
-// Route::get('/me', [AuthController::class, 'me']);
 // routes/api.php
 
 // Ruta solo para Developer
@@ -20,15 +15,16 @@ Route::middleware(['api', 'jwt.auth', 'role:Developer'])->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::resource('userData', UserDataController::class);
 });
 
 // Ruta para múltiples roles
-Route::middleware(['auth:api', 'role:Developer,Admin'])->group(function () {
+Route::middleware(['api', 'role:Developer,Admin'])->group(function () {
     // Route::get('/reports', [ReportController::class, 'index']);
     // Route::get('/analytics', [AnalyticsController::class, 'index']);
 });
 
 // Ruta pública (sin verificación de rol)
-Route::middleware('auth:api')->group(function () {
+Route::middleware('api')->group(function () {
     // Route::get('/profile', [ProfileController::class, 'show']);
 });
