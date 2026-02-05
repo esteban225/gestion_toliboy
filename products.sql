f40933bacb0a11e096800ca445e4bd02b51d5064
esteban225 <esteban.ricardo.dev@gmail.com>
2026-02-05 16:34:16 -0500

    Agregar inserciones de productos para las categorías de pastelería y panadería en los archivos products.sql y rawmaterials.sql
    Por: Esteban David Ricardo Fuentes
    
    Fecha: 05/02/2026
    
    version: 0.26.42

---
+++ products.sql
@@ -0,0 +1,67 @@
+-- CATEGORÍA: PASTELERÍA
+INSERT INTO toliboy.products (name, code, category, description, specifications, unit_price, is_active, created_by) VALUES
+('Brazo', 'PAST001', 'Pastelería', 'Producto de pastelería', JSON_ARRAY('harina', 'azucar', 'lecitina', 'emulsol', 'nucleo', 'goma guar', 'propianato de sodio', 'acido sorbico', 'sorbato', 'sal', 'bicarbonato sodio', 'bicarbonato de amonio', 'goma xanta', 'accido acetico', 'acido lactico', 'esencia', 'polvo horner'), 0.00, 1, NULL),
+
+('Torta', 'PAST002', 'Pastelería', 'Producto de pastelería', JSON_ARRAY('harina', 'azucar', 'margarina', 'lecitina', 'emulsol', 'nucleo', 'goma guar', 'propianato de sodio', 'acido sorbico', 'sorbato', 'sal', 'goma xanta', 'accido acetico', 'acido lactico', 'aceite', 'esencia', 'polvo horner'), 0.00, 1, NULL),
+
+('Junior', 'PAST003', 'Pastelería', 'Producto de pastelería', JSON_ARRAY('harina', 'azucar', 'mixo', 'nucleo', 'goma guar', 'propianato de sodio', 'acido sorbico', 'sorbato', 'sal', 'accido acetico', 'acido lactico', 'suero', 'aceite', 'esencia', 'polvo horner'), 0.00, 1, NULL),
+
+('Yoyo', 'PAST004', 'Pastelería', 'Producto de pastelería', JSON_ARRAY('harina', 'azucar', 'mixo', 'goma guar', 'propianato de sodio', 'acido sorbico', 'sorbato', 'sal', 'accido acetico', 'levadura', 'esencia', 'polvo horner'), 0.00, 1, NULL),
+
+('Mantecada Domo', 'PAST005', 'Pastelería', 'Producto de pastelería', JSON_ARRAY('harina', 'azucar', 'lecitina', 'emulsol', 'nucleo', 'goma guar', 'propianato de sodio', 'acido sorbico', 'sorbato', 'sal', 'goma xanta', 'accido acetico', 'acido lactico', 'aceite', 'harina de maiz', 'esencia', 'polvo horner'), 0.00, 1, NULL),
+
+('Mantecada Artesanal', 'PAST006', 'Pastelería', 'Producto de pastelería', JSON_ARRAY('harina', 'azucar', 'lecitina', 'emulsol', 'nucleo', 'goma guar', 'propianato de sodio', 'acido sorbico', 'sorbato', 'sal', 'goma xanta', 'accido acetico', 'acido lactico', 'aceite', 'harina de maiz', 'esencia', 'polvo horner'), 0.00, 1, NULL),
+
+('Tajada Sabores', 'PAST007', 'Pastelería', 'Producto de pastelería', JSON_ARRAY('harina', 'azucar', 'mixo', 'lecitina', 'emulsol', 'nucleo', 'goma guar', 'propianato de sodio', 'acido sorbico', 'sorbato', 'sal', 'goma xanta', 'accido acetico', 'acido lactico', 'suero', 'aceite', 'esencia', 'polvo horner'), 0.00, 1, NULL),
+
+('Chocorrico', 'PAST008', 'Pastelería', 'Producto de pastelería', JSON_ARRAY('harina', 'azucar', 'mixo', 'lecitina', 'emulsol', 'nucleo', 'goma guar', 'propianato de sodio', 'acido sorbico', 'sorbato', 'sal', 'goma xanta', 'accido acetico', 'acido lactico', 'suero', 'aceite', 'esencia', 'polvo horner'), 0.00, 1, NULL),
+
+('Repolla', 'PAST009', 'Pastelería', 'Producto de pastelería', JSON_ARRAY('harina', 'azucar', 'margarina', 'sorbato', 'sal', 'hidrogenado', 'Fecula'), 0.00, 1, NULL),
+
+('Torta Queso', 'PAST010', 'Pastelería', 'Producto de pastelería', JSON_ARRAY('harina', 'azucar', 'margarina', 'mixo', 'acido sorbico', 'sorbato', 'sal', 'accido acetico', 'acido lactico', 'esencia', 'polvo horner'), 0.00, 1, NULL),
+
+('Mini Chocorrico', 'PAST011', 'Pastelería', 'Producto de pastelería', JSON_ARRAY('harina', 'azucar', 'mixo', 'lecitina', 'emulsol', 'nucleo', 'goma guar', 'propianato de sodio', 'acido sorbico', 'sorbato', 'sal', 'goma xanta', 'accido acetico', 'acido lactico', 'suero', 'aceite', 'esencia', 'polvo horner'), 0.00, 1, NULL);
+
+-- CATEGORÍA: PANADERÍA
+INSERT INTO toliboy.products (name, code, category, description, specifications, unit_price, is_active, created_by) VALUES
+('Tajado Blanco', 'PAN001', 'Panadería', 'Producto de panadería', JSON_ARRAY('harina', 'azucar', 'margarina', 'mixo', 'levadura', 'propianato de calcio', 'acido citrico', 'sal', 'goma xanta', 'accido acetico', 'esencia'), 0.00, 1, NULL),
+
+('Tajado Mantequilla', 'PAN002', 'Panadería', 'Producto de panadería', JSON_ARRAY('harina', 'azucar', 'margarina', 'mixo', 'levadura', 'propianato de calcio', 'acido citrico', 'sal', 'goma xanta', 'accido acetico', 'esencia'), 0.00, 1, NULL),
+
+('Tajado Integral', 'PAN003', 'Panadería', 'Producto de panadería', JSON_ARRAY('harina', 'azucar', 'margarina', 'mixo', 'levadura', 'propianato de calcio', 'acido citrico', 'sal', 'goma xanta', 'accido acetico', 'esencia'), 0.00, 1, NULL),
+
+('Tajado Blanco Mini', 'PAN004', 'Panadería', 'Producto de panadería', JSON_ARRAY('harina', 'azucar', 'margarina', 'mixo', 'levadura', 'propianato de calcio', 'acido citrico', 'sal', 'goma xanta', 'accido acetico', 'esencia'), 0.00, 1, NULL),
+
+('Tostada', 'PAN005', 'Panadería', 'Producto de panadería', JSON_ARRAY('harina', 'azucar', 'margarina', 'levadura', 'harina de maiz', 'propianato de calcio', 'sal', 'esencia'), 0.00, 1, NULL),
+
+('Hojaldre', 'PAN006', 'Panadería', 'Producto de panadería', JSON_ARRAY('harina', 'azucar', 'margarina', 'propianato de calcio', 'sal', 'esencia'), 0.00, 1, NULL),
+
+('Pan Leche', 'PAN007', 'Panadería', 'Producto de panadería', JSON_ARRAY('harina', 'azucar', 'margarina', 'levadura', 'harina de maiz', 'propianato de calcio', 'suero', 'acido citrico', 'sal', 'goma xanta', 'accido acetico', 'esencia'), 0.00, 1, NULL),
+
+('Pan Queso', 'PAN008', 'Panadería', 'Producto de panadería', JSON_ARRAY('harina', 'azucar', 'margarina', 'levadura', 'harina de maiz', 'propianato de calcio', 'acido citrico', 'sal', 'goma xanta', 'accido acetico', 'esencia'), 0.00, 1, NULL),
+
+('Pan de Coco', 'PAN009', 'Panadería', 'Producto de panadería', JSON_ARRAY('harina', 'azucar', 'margarina', 'levadura', 'propianato de calcio', 'acido citrico', 'sal', 'goma xanta', 'accido acetico', 'esencia'), 0.00, 1, NULL),
+
+('Pan Perro', 'PAN010', 'Panadería', 'Producto de panadería', JSON_ARRAY('harina', 'azucar', 'margarina', 'levadura', 'propianato de calcio', 'acido citrico', 'sal', 'goma xanta', 'accido acetico', 'esencia'), 0.00, 1, NULL),
+
+('Hamburguesa Burguer', 'PAN011', 'Panadería', 'Producto de panadería', JSON_ARRAY('harina', 'azucar', 'margarina', 'levadura', 'harina de maiz', 'propianato de calcio', 'acido citrico', 'sal', 'goma xanta', 'accido acetico', 'esencia'), 0.00, 1, NULL),
+
+('Pan Piña', 'PAN012', 'Panadería', 'Producto de panadería', JSON_ARRAY('harina', 'azucar', 'margarina', 'levadura', 'propianato de calcio', 'acido citrico', 'sal', 'goma xanta', 'accido acetico', 'esencia'), 0.00, 1, NULL),
+
+('Hamburguesa Toliboy', 'PAN013', 'Panadería', 'Producto de panadería', JSON_ARRAY('harina', 'azucar', 'margarina', 'levadura', 'harina de maiz', 'propianato de calcio', 'acido citrico', 'sal', 'goma xanta', 'accido acetico', 'esencia'), 0.00, 1, NULL),
+
+('Calentano', 'PAN014', 'Panadería', 'Producto de panadería', JSON_ARRAY('harina', 'azucar', 'margarina', 'levadura', 'propianato de calcio', 'acido citrico', 'sal', 'goma xanta', 'accido acetico', 'esencia'), 0.00, 1, NULL),
+
+('Mogolla Miel', 'PAN015', 'Panadería', 'Producto de panadería', JSON_ARRAY('harina', 'azucar', 'margarina', 'levadura', 'propianato de calcio', 'acido citrico', 'sal', 'goma xanta', 'accido acetico', 'esencia'), 0.00, 1, NULL),
+
+('Pan de 100g', 'PAN016', 'Panadería', 'Producto de panadería', JSON_ARRAY('harina', 'azucar', 'margarina', 'levadura', 'propianato de calcio', 'acido citrico', 'sal', 'goma xanta', 'accido acetico', 'esencia'), 0.00, 1, NULL),
+
+('Panecitos', 'PAN017', 'Panadería', 'Producto de panadería', JSON_ARRAY('harina', 'azucar', 'margarina', 'levadura', 'propianato de calcio', 'acido citrico', 'sal', 'goma xanta', 'accido acetico', 'esencia'), 0.00, 1, NULL),
+
+('Mogolla Anis', 'PAN018', 'Panadería', 'Producto de panadería', JSON_ARRAY('harina', 'azucar', 'margarina', 'levadura', 'propianato de calcio', 'acido citrico', 'sal', 'goma xanta', 'accido acetico', 'esencia'), 0.00, 1, NULL),
+
+('Perro Brioche', 'PAN019', 'Panadería', 'Producto de panadería', JSON_ARRAY('harina', 'azucar', 'margarina', 'levadura', 'propianato de calcio', 'acido citrico', 'sal', 'goma xanta', 'accido acetico', 'esencia'), 0.00, 1, NULL),
+
+('Integral Mini', 'PAN020', 'Panadería', 'Producto de panadería', JSON_ARRAY('harina', 'azucar', 'margarina', 'levadura', 'propianato de calcio', 'acido citrico', 'sal', 'goma xanta', 'accido acetico', 'esencia'), 0.00, 1, NULL),
+
+('Pan Consorcio', 'PAN021', 'Panadería', 'Producto de panadería', JSON_ARRAY('harina', 'azucar', 'margarina', 'levadura', 'propianato de calcio', 'sal', 'esencia'), 0.00, 1, NULL);
